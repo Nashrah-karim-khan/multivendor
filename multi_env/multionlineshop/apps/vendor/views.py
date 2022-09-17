892689fb02607fb8fa34eac7d7b1d900073308c8
mewradha18 <19101018@uap-bd.edu>
2022-09-17 14:25:13 +0600

    version3.2
    more of vendor and product things added in html

--- multi_env/multionlineshop/apps/vendor/views.py
+++ multi_env/multionlineshop/apps/vendor/views.py
@@ -1,9 +1,11 @@
+from itertools import product
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
 from .models import Vendor
+from apps.product.models import Product
  
 def become_vendor(request):
     if request.method == 'POST':
@@ -26,5 +28,8 @@ def become_vendor(request):
 @login_required
 def vendor_admin(request):
     vendor = request.user.vendor
+    products = vendor.products.all()
 
-    return render(request, 'vendor/vendor_admin.html', {'vendor': vendor})
+    products = vendor.products.all()
+    products = vendor.products.all()
+    return render(request, 'vendor/vendor_admin.html', {'vendor': vendor, 'products': products})
