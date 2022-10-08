c8cf2960d1408796b7af3a68f462aa2091b9ca3f
mewradha18 <19101018@uap-bd.edu>
2022-10-09 00:20:46 +0600

    version-3.8-nashrah
    did some changes in product views.py file n now it runs again

--- multi_env/multionlineshop/apps/product/views.py
+++ multi_env/multionlineshop/apps/product/views.py
@@ -1,9 +1,13 @@
 import random
 
+from django.contrib import messages
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
+from .forms import AddToCartForm
 from .models import Category, Product
+
+from apps.cart.cart import Cart
 
 def search(request):
     query = request.GET.get('query', '')
@@ -12,7 +16,21 @@ def search(request):
     return render(request, 'product/search.html', {'products': products, 'query': query})
 
 def product(request, category_slug, product_slug):
+    cart = Cart(request) 
+
     product = get_object_or_404(Product, category__slug=category_slug, slug=product_slug)
+    
+    if request.method == 'POST':
+        form = AddToCartForm(request.POST)
+
+        if form.is_valid():
+            quantity = form.cleaned_data['quantity']
+
+            cart.add(product_id=product.id, quantity=quantity, update_quantity= False)
+
+            messages.success(request, 'The product was added to the cart')
+            
+            return redirect('product', category_slug=category_slug, product_slug= product_slug)
 
     similar_products = list(product.category.products.exclude(id=product.id))
 
