1652e8d24f257a951eaec47385391c170149132e
rahatmb <89215260+rahatmb@users.noreply.github.com>
2022-10-09 23:08:16 +0600

    version-7-rahat
    updated vendor models.py

--- multi_env/multionlineshop/apps/vendor/models.py
+++ multi_env/multionlineshop/apps/vendor/models.py
@@ -1,5 +1,4 @@
-
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
 from django.db import models
 
 class Vendor(models.Model):
@@ -7,9 +6,13 @@ class Vendor(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.OneToOneField(User, related_name='vendor', on_delete=models.CASCADE)
 
-    class Meta :
+    class Meta:
         ordering = ['name']
     
     def __str__(self):
-        return self.name 
-   
+        return self.name
+    
+    def get_balance(self):
+        items = self.items.filter(vendor_paid=False, order__vendors__in=[self.id])
+        return sum((item.product.price * item.quantity) for item in items)
+    
