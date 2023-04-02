672ff9a69fa2fcc57579e41ba5025334af1045fa
mewradha18 <59606277+al-amin-anik@users.noreply.github.com>
2023-04-03 04:26:15 +0600

    anik update core views.py3

--- multi_env/multionlineshop/apps/core/views.py
+++ multi_env/multionlineshop/apps/core/views.py
@@ -2,10 +2,9 @@ from django.shortcuts import render
 
 from apps.product.models import Product
 
-
 def frontpage(request):
     newest_products = Product.objects.all()[0:8]
-    
+
 
     return render(request, 'core/frontpage.html', {'newest_products': newest_products})
 
